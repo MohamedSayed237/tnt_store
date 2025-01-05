e5e1ca142fcaf5f0fc82a2c31f6d4de322e4088a
MohamedSayed237 <mohamed.sayed23702@gmail.com>
2025-01-05 22:52:40 +0200

    test

--- lib/features/onboarding/presentation/views/on_boarding_view.dart
+++ lib/features/onboarding/presentation/views/on_boarding_view.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/on_boarding_view_body.dart';
+
 class OnBoardingView extends StatelessWidget {
   const OnBoardingView({super.key});
   static const String routeName = 'onboarding';
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(body: OnBoardingViewBody());
   }
 }
