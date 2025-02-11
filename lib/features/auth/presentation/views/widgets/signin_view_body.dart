ed7c92970c96ffcbc641e144f05ddfae9b5ba6be
MohamedSayed237 <mohamed.sayed23702@gmail.com>
2025-02-11 18:55:03 +0200

    login with email and password

--- lib/features/auth/presentation/views/widgets/signin_view_body.dart
+++ lib/features/auth/presentation/views/widgets/signin_view_body.dart
@@ -1,43 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tnt_store/constants.dart';
 import 'package:tnt_store/core/utils/app_colors.dart';
 import 'package:tnt_store/core/utils/app_images.dart';
 import 'package:tnt_store/core/utils/app_text_styles.dart';
 import 'package:tnt_store/core/widgets/custom_button.dart';
 import 'package:tnt_store/core/widgets/custom_text_form_field.dart';
+import 'package:tnt_store/core/widgets/password_field.dart';
+import 'package:tnt_store/features/auth/presentation/cubits/signin_cubit/signin_cubit.dart';
 
 import 'dont_have_an_account_widget.dart';
 import 'or_divider.dart';
 import 'social_login_button.dart';
 
-class SigninViewBody extends StatelessWidget {
+class SigninViewBody extends StatefulWidget {
   const SigninViewBody({super.key});
 
   @override
+  State<SigninViewBody> createState() => _SigninViewBodyState();
+}
+
+class _SigninViewBodyState extends State<SigninViewBody> {
+  late String email, password;
+  AutovalidateMode autoValidateMode = AutovalidateMode.disabled;
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
-        child: SingleChildScrollView(
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
+      child: SingleChildScrollView(
+        child: Form(
+          key: formKey,
+          autovalidateMode: autoValidateMode,
           child: Column(
             children: [
               const SizedBox(
                 height: 24,
               ),
-              const CustomTextFormField(
+              CustomTextFormField(
+                onSaved: (value) {
+                  email = value!;
+                },
                 hintText: 'البريد الإلكتروني',
                 textInputType: TextInputType.emailAddress,
               ),
               const SizedBox(
                 height: 16,
               ),
-              const CustomTextFormField(
-                hintText: 'كلمة المرور',
-                textInputType: TextInputType.visiblePassword,
-                suffixIcon: Icon(
-                  Icons.remove_red_eye_outlined,
-                  color: Color(0xffC9CECF),
-                ),
+              PasswordField(
+                onSaved: (value) {
+                  password = value!;
+                },
               ),
               const SizedBox(
                 height: 16,
@@ -58,7 +72,18 @@ class SigninViewBody extends StatelessWidget {
               ),
               CustomButton(
                 text: 'تسجيل الدخول',
-                onPressed: () {},
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    formKey.currentState!.save();
+                    context.read<SigninCubit>().signInWithEmailAndPassword(
+                          email,
+                          password,
+                        );
+                  } else {
+                    autoValidateMode = AutovalidateMode.always;
+                    setState(() {});
+                  }
+                },
               ),
               const SizedBox(
                 height: 33,
--- lib/features/auth/presentation/views/widgets/signin_view_body_bloc_consumer.dart
+++ lib/features/auth/presentation/views/widgets/signin_view_body_bloc_consumer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tnt_store/core/healper_functions/build_error_bar.dart';
 import 'package:tnt_store/core/widgets/custom_progress_hud.dart';
 import 'package:tnt_store/features/auth/presentation/cubits/signin_cubit/signin_cubit.dart';
 import 'package:tnt_store/features/auth/presentation/views/widgets/signin_view_body.dart';
@@ -12,7 +13,12 @@ class SigninViewBodyBlocConsumer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<SigninCubit, SigninState>(
-      listener: (context, state) {},
+      listener: (context, state) {
+        if (state is SigninFailure) {
+          buildErrorBar(context, state.message);
+        }
+        if (state is SigninSuccess) {}
+      },
       builder: (context, state) {
         return CustomProgressHud(
           isLoading: state is SigninLoading ? true : false,
